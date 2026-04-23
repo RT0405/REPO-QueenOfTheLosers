83ad739839142fe2f9ade15acfe003458153c2b6
RT0405 <66913062+RT0405@users.noreply.github.com>
2026-04-22 20:36:16 -0500

    1.0.0

---
+++ src/Plugin.cs
@@ -0,0 +1,99 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using REPOLib.Modules;
+using System;
+using UnityEngine;
+
+namespace QueenOfTheLosers;
+
+[BepInPlugin("QueenOfTheLosers", "Queen Of The Losers", "1.0.0")]
+[BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+[BepInDependency("nickklmao-REPOConfig-1.2.3", BepInDependency.DependencyFlags.SoftDependency)]
+public class QueenOfTheLosers : BaseUnityPlugin
+{
+    internal static QueenOfTheLosers Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+#pragma warning disable IDE1006 // Naming Styles
+    private ManualLogSource _logger => base.Logger;
+#pragma warning restore IDE1006 // Naming Styles
+    internal Harmony? Harmony { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    internal NetworkedEvent SyncTitle;
+    internal ConfigEntry<string> TitleConfig;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+    public const string DefaultTitle = "RULER";
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        Patch();
+
+        TitleConfig = Config.Bind("General", "Title Selection", DefaultTitle, new ConfigDescription("A replacement for the title of KING", new AcceptableValueList<string>("QUEEN", "RULER", "KING", "MONARCH")));
+        TitleConfig.SettingChanged += (_, _) =>
+        {
+            PlayerTitleHandler.LocalTitle = TitleConfig.Value;
+        };
+        PlayerTitleHandler.LocalTitle = TitleConfig.Value;
+
+        SyncTitle = new("SyncPreferredPlayerTitle", (e) =>
+        {
+            if(!(e.CustomData is string text && TryParseMessage(text, out var message)))
+            {
+                Logger.LogWarning("Recieved malformed player title sync message, ignoring");
+                return;
+            }
+
+            PlayerTitleHandler.SetTitle(message.playerID, message.title);
+            //Logger.LogInfo($"Setting Title for {message.playerID} to \"{message.title}\"");
+        });
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+    public static bool TryParseMessage(string message, out (string playerID, string title) value)
+    {
+        value = default;
+
+        int index = message.IndexOf(':');
+
+        if(index == -1) return false;
+
+        // no need to allocate actual backing strings until we've validated the input
+        ReadOnlySpan<char> playerID = message.AsSpan()[..index].Trim();
+        ReadOnlySpan<char> title = message.AsSpan()[(index + 1)..].Trim();
+
+        // if the client sends an empty or large title or playerID, ignore it, it's malformed, and either a bug or malicious behavior.
+        if(playerID.Length == 0 || title.Length == 0) return false;
+        if(playerID.Length > 64 || title.Length > 32) return false;
+
+        // make sure there are no crazy characters
+        for(int i = 0; i < title.Length; i++)
+        {
+            char c = title[i];
+            if(!char.IsLetterOrDigit(c) && c != ' ' && c != '-' && c != '_')
+            {
+                return false;
+            }
+        }
+
+        value = (new(playerID), new(title));
+        return true;
+    }
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+}
---
+++ src/plugins/QueenOfTheLosers/README.md
@@ -0,0 +1,13 @@
+# Queen of the Losers
+
+Allows Configuration of the \"King of the Losers\" screen to show other titles, such as Queen / Ruler.
+
+Syncs choice to other clients who also have the mod so the correct title is shown to everyone.
+
+Use the [REPOConfig](https://thunderstore.io/c/repo/p/nickklmao/REPOConfig/) mod to change the title while in game.
+
+If the host has the mod, titles will be synced with all clients who also have the mod. 
+If the host does not have the mod, clients will only see their own title, and will be shown to everyone else as King.
+
+Currently (due to limitations in the config api) I have to define each title that can be used. If you want more to be added, let me know and I can add them in a future update.
+The following titles are currently available: Queen, Ruler, King, Monarch.
